1f3c5b9c0511d2be175ddb2a8f18fd6ea93e6e5e
abe-tetsu <abetetsuya@appify-inc.com>
2023-03-15 15:38:52 +0900

    fix

--- myanalyzer/testdata/src/a/a.go
+++ myanalyzer/testdata/src/a/a.go
@@ -4,9 +4,17 @@ import "fmt"
 
 // for 文を見つける
 func findFor() {
-	for findForVar := 0; findForVar < 3; findForVar++ { // want "for found"
-		fmt.Println(findForVar)
+	for findForVar := 0; findForVar < 3; findForVar++ {
+		fmt.Println(findForVar) // want "for found"
 	}
 }
 
-//
+func pointer() {
+	for findForVar := 0; findForVar < 3; findForVar++ {
+		fmt.Println(&findForVar) // want "and used in for"
+	}
+
+	for findForVar := 0; findForVar < 3; findForVar++ {
+		fmt.Println(findForVar) // OK
+	}
+}
